e65472c0488b7962e84b5af4a3a0246da575e788
Andrew <acasta4704@gmail.com>
2021-03-28 18:16:47 +1100

    add models

--- network/models.py
+++ network/models.py
@@ -3,4 +3,38 @@ from django.db import models
 
 
 class User(AbstractUser):
+    """"""
+
     pass
+
+
+class Post(models.Model):
+    """
+    post copy
+    post date
+    poster (user)
+    """
+
+    post = models.CharField(max_length=280)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    datetime = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+
+class Follow(models.Model):
+    """
+    user (fk)
+    following (fk)
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follows")
+    follow = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    """
+    user (fk)
+    post (fk)
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="liked_by")
